28f34ad6cb86df901afa8f6adeacaf2b790165b8
Eliot Rodriguez <mr.ear84@gmail.com>
2019-06-11 14:35:43 -0400

    updating html css for the project

--- assets/js/rps.js
+++ assets/js/rps.js
@@ -1,1 +1,9 @@
 console.log("rps.js is online");
+
+
+      var p1 = document.getElementById("p1");
+
+
+        p1.addEventListener("click", function(){
+            console.log("Hello!! you clicked me!");
+      });
